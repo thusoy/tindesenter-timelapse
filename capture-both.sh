84fb0d5922422e1ae59b874551f880212b427a9b
Tarjei Husøy <thusoy@users.noreply.github.com>
2014-12-25 14:51:38 +0100

    Crop images on the pi
    Also switch to raspistill to capture from the picamera, instead of using python.

--- capture-both.sh
+++ capture-both.sh
@@ -3,9 +3,25 @@
 # Fail script if any command fails
 set -e
 
+# Where to store the images locally (without trailing slash)
+target_dir="/tmp"
+
 fileid=$(date +"%s")
 
-/usr/local/bin/gphoto2 --capture-image-and-download --filename "/tmp/nikon-$fileid.jpg" 2>&1
-python picam.py "/tmp/picam-$fileid.jpg"
-scp /tmp/nikon-$fileid.jpg /tmp/picam-$fileid.jpg pengebingen:~/tarjei/Foto/Timelapse/Tindesenteret/RAW
-rm /tmp/*-$fileid.jpg
+# Capture the images
+/usr/local/bin/gphoto2 --capture-image-and-download --filename "$target_dir/nikon-$fileid.jpg" 2>&1
+raspistill -q 100 -n -t 1 -awb cloud -o "$target_dir/picam-$fileid.jpg"
+
+# Crop away some cruft
+echo "Cropping nikon..."
+convert $target_dir/nikon-$fileid.jpg -crop +480+250 -crop -335+0 -quality 100 $target_dir/nikon-cropped-$fileid.jpg
+echo "Cropping picam..."
+convert $target_dir/picam-$fileid.jpg -crop +85+0 -crop -90+0 -quality 100 $target_dir/picam-cropped-$fileid.jpg
+
+# Push images to storage
+echo "Uploading images..."
+scp $target_dir/*-$fileid.jpg pengebingen:~/tarjei/Foto/Timelapse/Tindesenteret/RAW
+
+# If everything has succeeded so far, remove the local copies
+echo "Deleting local copies..."
+rm $target_dir/*-$fileid.jpg
